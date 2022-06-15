8e6d58b54ee8005b88a3dfcfd07b1be4cc875f3d
João Lourenço <joaodslourenco@gmail.com>
2022-06-15 13:25:08 -0300

    chore: first routes

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,17 @@ import express from "express";
 
 const app = express();
 
-const books = [];
+const books = [
+  { id: 1, titulo: "senhor dos aneis" },
+  { id: 2, titulo: "O Hobbit" },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de node");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
+});
+
+export default app;
