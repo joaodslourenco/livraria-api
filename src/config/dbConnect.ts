b5e15d499c7ba1830fb6acabf3aeac65edfc2e1c
João Lourenço <joaodslourenco@gmail.com>
2022-08-03 14:43:19 -0300

    feat: creates local db when env key is missing

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-mongoose.connect(`${process.env.MONGO_URL}`);
+mongoose.connect(
+  `${process.env.MONGO_URL || "mongodb://localhost:27017/livraria"}`,
+);
 
 const db = mongoose.connection;
 
